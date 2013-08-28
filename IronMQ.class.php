630bed647f2bfd02f4596d73e93ca99c76b98fbb
thousandsofthem <thousandsofthem@gmail.com>
2013-08-28 18:37:19 +0300

    version bump

--- IronMQ.class.php
+++ IronMQ.class.php
@@ -6,7 +6,7 @@
  * @link https://github.com/iron-io/iron_mq_php
  * @link http://www.iron.io/products/mq
  * @link http://dev.iron.io/
- * @version 1.4.5
+ * @version 1.4.6
  * @package IronMQPHP
  * @copyright Feel free to copy, steal, take credit for, or whatever you feel like doing with this code. ;)
  */
@@ -122,7 +122,7 @@ class IronMQ_Message {
 
 class IronMQ extends IronCore {
 
-    protected $client_version = '1.4.5';
+    protected $client_version = '1.4.6';
     protected $client_name    = 'iron_mq_php';
     protected $product_name   = 'iron_mq';
     protected $default_values = array(
