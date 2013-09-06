f658e721cb6eb7d4c64932e00ed4add742980ff9
thousandsofthem <thousandsofthem@gmail.com>
2013-09-06 15:55:39 +0300

    hide notice for composer environment, bump version

--- IronMQ.class.php
+++ IronMQ.class.php
@@ -6,13 +6,13 @@
  * @link https://github.com/iron-io/iron_mq_php
  * @link http://www.iron.io/products/mq
  * @link http://dev.iron.io/
- * @version 1.4.6
+ * @version 1.4.7
  * @package IronMQPHP
  * @copyright Feel free to copy, steal, take credit for, or whatever you feel like doing with this code. ;)
  */
 
 
-if (!class_exists('IronCore')) {
+if (!class_exists('IronCore') && !class_exists('Composer\Autoload\ClassLoader')) {
     echo "Please include IronCore class first\n";
     return;
 }
@@ -122,7 +122,7 @@ class IronMQ_Message {
 
 class IronMQ extends IronCore {
 
-    protected $client_version = '1.4.6';
+    protected $client_version = '1.4.7';
     protected $client_name    = 'iron_mq_php';
     protected $product_name   = 'iron_mq';
     protected $default_values = array(
