bc27b0ff8ad2b4671ade398506b7ed552e9da3a3
thousandsofthem <thousandsofthem@gmail.com>
2015-07-24 00:55:05 +0300

    update versions

--- src/IronMQ.php
+++ src/IronMQ.php
@@ -6,7 +6,7 @@
  * @link https://github.com/iron-io/iron_mq_php
  * @link http://www.iron.io/products/mq
  * @link http://dev.iron.io/
- * @version 3.0.3
+ * @version 4.0.0
  * @package IronMQPHP
  * @copyright Feel free to copy, steal, take credit for, or whatever you feel like doing with this code. ;)
  */
@@ -15,17 +15,6 @@ namespace IronMQ;
 
 use IronCore\IronCore;
 
-/**
- * PHP client for IronMQ
- * IronMQ is a scalable, reliable, high performance message queue in the cloud.
- *
- * @link https://github.com/iron-io/iron_mq_php
- * @link http://www.iron.io/products/mq
- * @link http://dev.iron.io/
- * @version 4.0.0
- * @package IronMQPHP
- * @copyright Feel free to copy, steal, take credit for, or whatever you feel like doing with this code. ;)
- */
 class IronMQ extends IronCore
 {
 
--- tests/IronMQTest/IronMQExceptionTest.php
+++ tests/IronMQTest/IronMQExceptionTest.php
@@ -6,7 +6,6 @@
  * @link https://github.com/iron-io/iron_mq_php
  * @link http://www.iron.io/products/mq
  * @link http://dev.iron.io/
- * @version 3.0.3
  * @package IronMQPHP
  * @copyright Feel free to copy, steal, take credit for, or whatever you feel like doing with this code. ;)
  */
